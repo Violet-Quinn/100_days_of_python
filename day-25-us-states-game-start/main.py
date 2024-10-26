ba34da289d7e54fad6bef2c3fb71b03ffd05ba60
Violet Quinn <140976001+Violet-Quinn@users.noreply.github.com>
2024-10-26 19:53:07 +0530

    us states game code updated, used list comprehension for reduce code

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -1,6 +1,4 @@
 import turtle
-from multiprocessing.connection import answer_challenge
-
 import pandas
 
 
@@ -22,13 +20,14 @@ all_states=data.state.to_list()
 guessed_states=[]
 
 while len(guessed_states)<50:
-    answer_state=screen.textinput(title="Guess the state",prompt="What's another state's name?").title()
+    answer_state=screen.textinput(title=f"{len(guessed_states)}/50 States correct",prompt="What's another state's name?").title()
     print(answer_state)
     if answer_state=="Exit":
-        missing_states=[]
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states=[state for state in all_states if state not in guessed_states]
+        # missing_states=[]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data=pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
