11eab7ec45db8a681a04fb44415c937b517b0b02
Violet Quinn <140976001+Violet-Quinn@users.noreply.github.com>
2024-11-11 09:21:16 +0530

    day 30 complete, added exception handling in NATO alphabet main and password manager, also added search functionality in password manager.

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,8 +1,11 @@
+from json import JSONDecodeError
 from tkinter import *
 from tkinter import messagebox
 from PIL import Image,ImageTk
 from random import choice,randint,shuffle
 import pyperclip
+import json
+
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -22,28 +25,57 @@ def generate_password():
     generated_password="".join(password_list)
     password_entry.insert(0,generated_password)
     pyperclip.copy(generated_password)
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def find_password():
+    website=website_entry.get()
+    try:
+        with open("saved_pass.json") as data_file:
+            data=json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found.")
+    if website in data:
+        email=data[website]["email"]
+        password=data[website]["password"]
+        messagebox.showinfo(title="Password Found",message=f"Email: {email}\nPassword: {password}")
+    else:
+        messagebox.showinfo(title="Error", message="No matches found.")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website=website_entry.get()
     email=email_entry.get()
     password=password_entry.get()
-    ok_to_save = messagebox.askokcancel(title="Website",
-                                        message=f"These are the details entered: \n"
-                                        f"Email: {email}\nPassword: {password}\nDo you want to save?")
-    if ok_to_save:
-        if len(password)==0 or len(email)==0 or len(website)==0:
-            messagebox.showinfo(title="Invalid Input", message="fields cannot be empty.")
-        else:
-            with open("saved_pass.txt","a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-                print("Password saved")
-        website_entry.delete(0,END)
-        password_entry.delete(0,END)
+    new_data={
+        website:{
+            "email":email,
+            "password":password,
+        }
+    }
+    if len(password)==0 or len(email)==0 or len(website)==0:
+        messagebox.showinfo(title="Invalid Input", message="fields cannot be empty.")
+    else:
+        try:
+            with open("saved_pass.json","r") as data_file:
+                try:
+                    data=json.load(data_file)
+                except JSONDecodeError:
+                    data={}
+        except FileNotFoundError:
+            data={}
+        data.update(new_data)
+        with open("saved_pass.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window=Tk()
-window.title("Password Generator")
+window.title("Password Manager")
 window.config(padx=50,pady=50)
 canvas=Canvas(width=200,height=200)
 
@@ -62,8 +94,8 @@ password_label=Label(text="Password:")
 password_label.grid(row=3,column=0)
 
 #Entries
-website_entry=Entry(width=38)
-website_entry.grid(row=1,column=1,columnspan=2)
+website_entry=Entry(width=21)
+website_entry.grid(row=1,column=1)
 website_entry.focus()
 email_entry=Entry(width=38)
 email_entry.grid(row=2,column=1,columnspan=2)
@@ -72,6 +104,8 @@ password_entry=Entry(width=21)
 password_entry.grid(row=3,column=1)
 
 #Buttons
+search_button=Button(text="Search",width=13,command=find_password)
+search_button.grid(row=1,column=2)
 generate_password_button=Button(text="Generate Password",width=13,command=generate_password)
 generate_password_button.grid(row=3,column=2)
 add_button=Button(text="Add",width=36,command=save_password)
